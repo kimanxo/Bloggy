71d40ca1d7eea0b910ace9454b4f6eb088e1d0af
kimanxo <kimanxo@yandex.ru>
2024-06-17 16:17:00 +0100

    added navigation for upvoted and downvoted posts

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from .views import (
+    DownvotedPostsView,
     IndexView,
     BlogView,
     CategoryPostsView,
@@ -11,6 +12,7 @@ from .views import (
     SavedPostsView,
     SettingsView,
     ReadingListView,
+    UpvotedPostsView,
 )
 
 
@@ -26,4 +28,6 @@ urlpatterns = [
     path("accounts/settings", SettingsView.as_view(), name="settings"),
     path("accounts/reading_list", ReadingListView.as_view(), name="reading_list"),
     path("accounts/saved_posts", SavedPostsView.as_view(), name="saved_posts"),
+    path("accounts/upvoted_posts", UpvotedPostsView.as_view(), name="upvoted_posts"),
+    path("accounts/downvoted_posts", DownvotedPostsView.as_view(), name="downvoted_posts"),
 ]
